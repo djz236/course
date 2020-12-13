265b69b68c0fd7fa6614e9ff4f401219d7e15fae
djz236 <djz236@163.com>
2020-12-13 15:40:25 +0800

    1、增加日志格式

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -1,13 +1,23 @@
 package com.course;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
+private static final Logger LOG=LoggerFactory.getLogger(EurekaApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+
+		ConfigurableApplicationContext run = SpringApplication.run(EurekaApplication.class, args);
+		LOG.info("启动成功");
+		LOG.info("Eureka 地址：\thttp://127.0.0.1:{}",run.getEnvironment().getProperty("server.port"));
+
 	}
 
 }
