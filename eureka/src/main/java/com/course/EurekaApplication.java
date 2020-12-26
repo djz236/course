33ee9d423e2f877599d8f4386aff10fec4c4ce6b
djz236 <djz236@163.com>
2020-12-26 09:24:10 +0800

    1、新增gateway子模块，注册到注册中心

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -13,7 +13,6 @@ public class EurekaApplication {
 private static final Logger LOG=LoggerFactory.getLogger(EurekaApplication.class);
 
 	public static void main(String[] args) {
-
 		ConfigurableApplicationContext run = SpringApplication.run(EurekaApplication.class, args);
 		LOG.info("启动成功");
 		LOG.info("Eureka 地址：\thttp://127.0.0.1:{}",run.getEnvironment().getProperty("server.port"));
