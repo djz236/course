1b74388bab559a402237b31e1fb5318f64f90c9c
djz236 <djz236@163.com>
2021-01-03 11:41:30 +0800

    增加大章表 chapter

--- server/src/main/java/com/course/server/mapper/TestMapper.java
+++ server/src/main/java/com/course/server/mapper/TestMapper.java
@@ -10,13 +10,21 @@ public interface TestMapper {
 
     int deleteByExample(TestExample example);
 
+    int deleteByPrimaryKey(String id);
+
     int insert(Test record);
 
     int insertSelective(Test record);
 
     List<Test> selectByExample(TestExample example);
 
+    Test selectByPrimaryKey(String id);
+
     int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
 
     int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByPrimaryKeySelective(Test record);
+
+    int updateByPrimaryKey(Test record);
 }
