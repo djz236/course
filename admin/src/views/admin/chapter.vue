f27735456a0640efae5f26c68a4c1247d81c8627
djz236 <djz236@163.com>
2021-01-06 22:03:56 +0800

    1、增加大章的修改功能

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -29,62 +29,19 @@
             <td>{{chapter.courseId}}</td>
             <td>
               <div class="hidden-sm hidden-xs btn-group">
-                <button class="btn btn-xs btn-success">
-                  <i class="ace-icon fa fa-check bigger-120"></i>
-                </button>
-
-                <button class="btn btn-xs btn-info">
+                <button v-on:click="edit(chapter)"  class="btn btn-xs btn-info">
                   <i class="ace-icon fa fa-pencil bigger-120"></i>
                 </button>
-
                 <button class="btn btn-xs btn-danger">
                   <i class="ace-icon fa fa-trash-o bigger-120"></i>
                 </button>
-
-                <button class="btn btn-xs btn-warning">
-                  <i class="ace-icon fa fa-flag bigger-120"></i>
-                </button>
-              </div>
-
-              <div class="hidden-md hidden-lg">
-                <div class="inline pos-rel">
-                  <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-                    <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-                  </button>
-
-                  <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-                    <li>
-                      <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
-																			<span class="blue">
-																				<i class="ace-icon fa fa-search-plus bigger-120"></i>
-																			</span>
-                      </a>
-                    </li>
-
-                    <li>
-                      <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
-																			<span class="green">
-																				<i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
-																			</span>
-                      </a>
-                    </li>
-
-                    <li>
-                      <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
-																			<span class="red">
-																				<i class="ace-icon fa fa-trash-o bigger-120"></i>
-																			</span>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
               </div>
             </td>
           </tr>
           </tbody>
         </table>
 
-    <div class="modal fade" tabindex="-1" role="dialog">
+    <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
@@ -138,7 +95,13 @@
     methods:{
       add(){
         let _this=this;
-        $('.modal').modal("show");
+        _this.chapter={};
+        $('#form-modal').modal("show");
+      },
+      edit(chapter){
+        let _this=this;
+        _this.chapter=$.extend({},chapter);// chapter;
+        $('#form-modal').modal("show");
       },
       list(page){
         let _this=this;
@@ -147,15 +110,20 @@
           size:_this.$refs.pagination.size,
         }).then((response)=>{
               console.log("查询大章列表结果：",response);
-              _this.chapters=response.data.list;
-              _this.$refs.pagination.render(page,response.data.total);
+              let resp=response.data;
+              _this.chapters=resp.content.list;
+              _this.$refs.pagination.render(page,resp.content.total);
             });
       },
       save(){
         let _this=this;
         _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/save',
             _this.chapter).then((response)=>{
-
+          let resp=response.data;
+          if(resp.success){
+              $('#form-modal').modal("hide");
+              _this.list(1);
+          }
         });
       }
 
